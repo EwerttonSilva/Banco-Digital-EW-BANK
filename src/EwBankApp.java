692cfc57981f03978647fce038bdd0c99f91bf68
EwerttonSilva <ewertton.slv1@gmail.com>
2023-04-12 21:50:41 -0300

    Att EwBankApp

--- src/EwBankApp.java
+++ src/EwBankApp.java
@@ -2,16 +2,18 @@ import java.util.Scanner;
 
 public class EwBankApp {
     public static void main(String[] args) {
-        cadastroDeClientes cadastrando = new cadastroDeClientes();
+
+        cadastroDeClientes cadastrar = new cadastroDeClientes();
         Scanner input = new Scanner(System.in);
+
         // Iniciando o Projeto.
         System.out.println("====>> Seja Bem Vindo ao EW BANK <<====");
         System.out.print("|| Por gentileza, informe o seu nome: ");
         String nome = input.nextLine();
-
         System.out.print("Me Informe seu CPF: ");
         String cpfDoCliente = input.nextLine();
         String[] clientesCadastrados = {"70470961406", "70470961416"};
+
         boolean cpfValido = false;
         for (String cpf : clientesCadastrados) {
             if (cpf.equals(cpfDoCliente)) {
@@ -21,7 +23,7 @@ public class EwBankApp {
         }
         if (cpfValido) System.out.println("Olá Bem Vindo de Volta! " + nome);
         else {
-            cadastrando.clienteNovo(cpfDoCliente);
+            cadastrar.clienteNovo();
         }
     }
 }
